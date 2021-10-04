5dd3a668a67a54c799bfc18f56bbde811176d886
Pranav Shridhar <pranavmodx@gmail.com>
2021-10-04 20:00:31 +0530

    Initial Commit

---
+++ 2048/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2048
+//
+//  Created by Pranav Shridhar on 04/10/21.
+//  Copyright © 2021 ModX. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
